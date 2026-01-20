310a6a100bd2181644a756b6165b4f9deb9f0cb9
hh <coolage512@gmail.com>
2026-01-20 10:07:21 +0900

    console.log

--- server/api/pixabay.get.ts
+++ server/api/pixabay.get.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
   const { q, page } = getQuery(event)  // ?q=cat
   const config = useRuntimeConfig()
   const PIXABAY_KEY = config.privatePixabayKey
-
+console.log('Pixabay Key exists:', !!config.privatePixabayKey)
   const url = `https://pixabay.com/api/?key=${PIXABAY_KEY}&q=${q}&&page=${page}`
 
   const res = await fetch(url)
